6f338362acac7c2cbf8eea682ed1859364f8df10
divyaashree8 <divyaashree8@gmail.com>
2026-04-15 17:38:33 +0530

    feat: implement CLI period tracker using datetime module, added user input for last period date and cycle length, calculated next expected period, added ovulation estimation

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
-last_date_input=input("Enter last period date (YYY-MM-DD):")
+last_date_input=input("Enter last period date (YYYY-MM-DD):")
 cycle_length= int(input("Enter your cycle length (in days):"))
 # covert string to date
 last_date =datetime.strptime(last_date_input,"%Y-%m-%d")
 # calculate next period
 next_period=last_date+timedelta(days=cycle_length)
 # Display result
+#ovulation (approx 14 days before next period)
+ovulation=next_period-timedelta(days=14)
 print("\nNext Expected period date:",next_period.date())
+print("Estimated Ovulation:",ovulation.date())
