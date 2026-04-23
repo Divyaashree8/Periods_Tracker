ee2791d9623f7a9c35353d0a3888eaefa1291391
divyaashree8 <divyaashree8@gmail.com>
2026-04-23 17:47:06 +0530

    feat: add input validation, loop system and history file storage

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from datetime import datetime, timedelta
-last_date_input=input("Enter last period date (YYYY-MM-DD):")
-cycle_length= int(input("Enter your cycle length (in days):"))
-# covert string to date
-last_date =datetime.strptime(last_date_input,"%Y-%m-%d")
+def get_valid_date():
+    while True:
+        date_input=input("Enter last period date(YYYY-MM-DD):")
+        try:
+            return datetime.strptime(date_input,"%Y-%m-%d")
+        except ValueError:
+            print("Invalid Date format Try again.")
+def get_valid_number(prompt):
+    while True:
+        value=input(prompt)
+        if value.isdigit():
+            return int(value)
+        else:
+            print("please enter a valid number")
+def save_to_file(data):
+    with open("history.txt","a") as file:
+        file.write(data+"\n")
+def main():
+    print("\n Period Tracker")
+    while True:
+        last_date=get_valid_date()
+        cycle_length=get_valid_number("Enter Cycle length (days):")
 # calculate next period
-next_period=last_date+timedelta(days=cycle_length)
+        next_period=last_date+timedelta(days=cycle_length)
 # Display result
 #ovulation (approx 14 days before next period)
-ovulation=next_period-timedelta(days=14)
-print("\nNext Expected period date:",next_period.date())
-print("Estimated Ovulation:",ovulation.date())
+        ovulation=next_period-timedelta(days=14)
+        result= f"""
+last period:{last_date.date()}
+Next period:{next_period.date()}
+ovulation:{ovulation.date()}
+"""
+
+        print(result)
+        save_to_file(result)
+        choice=input("Do you want to track again?(Y/N):").lower()
+        if choice!='y':
+            print("Exiting.....Stay Healthy!")
+            break
+if __name__=="__main__":
+    main()
